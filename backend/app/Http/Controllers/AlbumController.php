8cbbff882dce0d53c70903c1060a23f765f31c3c
kosukeTK <74085542+kosukeTK@users.noreply.github.com>
2021-07-21 17:35:37 +0900

    Add files via upload

--- backend/app/Http/Controllers/AlbumController.php
+++ backend/app/Http/Controllers/AlbumController.php
@@ -8,20 +8,60 @@ use App\Models\Album;
 use App\Models\Album_file;
 
 class AlbumController extends Controller
-{
+{	
+
+	
     public function upload(Request $request)
-    {
-        //    $files = $request->file;  
+    {   
+        //日付を切り出し
+        $date  = $request->date
+        $year  = mb_substr($date, 4)
+        $month = mb_substr($date, 5, 2)
+        $day   = mb_substr($date, 8, 2)
+        
+        //album_no最大値
+        $album_no = $Album::max('album_no') + 1;
+        
+        //albumsテーブルINSERT
+        $album = $Album::forceCreate([
+            'year'		=> $year,
+            'month'		=> $month,
+            'day'		=> $day,
+            'user_id'	=> $request->id,
+            'album_no'	=> $album_no,
+            'title'		=> $request->title,
+            'comment'	=> $request->comment,
+        ]);
+        
         $files = $request->file('file');
         foreach ($files as $file) {
+        	//画像保存
             $file_name = $file->getClientOriginalName();
             $file_path = $file->storeAs('public', $file_name);
+            
+            //album_filesテーブルINSERT
+            $album_file = Album_file::forceCreate([
+            	'user_id'	=> $request->id,
+            	'album_no'	=> $album_no,
+            	'file_name'	=> $file_name,
+            	'file_path' => $file_path
+            ])
         }
+        $ret = ['album' => $album, 'album_file' => $album_file]
+		return $ret
+    }
 
+    public function get(Request $request) {
         $album = Album::Join('album_files', 'albums.user_id', '=', 'album_files.user_id')
-            ->where('album_files.album_no', '3')
+            ->where('album_files.user_id', $request->id)
             ->get();
         dd($album);
+        return $album;
+    }
+    
+    
+    
+    
         // $album =
         //     DB::insert(
         //         'insert into albums
@@ -54,6 +94,4 @@ class AlbumController extends Controller
         //             date("Y/m/d H:i:s")
         //         ]
         //     );
-        return ['album' => $album];
-    }
 }
